a8fab8010da07d4eab8acda36758af24ec37ac32
Bharat Singh <rajpurohitbharat513@gmail.com>
2024-01-08 21:46:05 +0530

    init

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
 const session = require("express-session");
 const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js")
+
+
 
 const sessionOptions = {
     secret: "mysupersecretcode",
@@ -29,9 +34,18 @@ app.get("/", (req, res) => {
 app.use(session(sessionOptions));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
 
-const listings = require("./routes/listing.js");
-const reviews = require("./routes/review.js");
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+
+const listingRouter = require("./routes/listing.js");
+const reviewRouter = require("./routes/review.js");
+const UserRouter = require("./routes/user.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -66,8 +80,19 @@ app.use((req, res, next) => {
 });
 
 
-app.use("/listings", listings);
-app.use("/listings/:id/reviews", reviews);
+// app.get("/demouser", async(req, res) => {
+//     let fakeUser = new User ({
+//         email: "student@gmail.com",
+//         username: "delta-student"
+//     });
+
+//     let registeredUser = await User.register(fakeUser, "helloworld");
+//     res.send(registeredUser);
+// });
+
+app.use("/listings", listingRouter);
+app.use("/listings/:id/reviews", reviewRouter);
+app.use("/", UserRouter);
 
 
 //server side error handler middleware
